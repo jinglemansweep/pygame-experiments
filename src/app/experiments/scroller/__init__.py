d08b4aae0490f659d5863bad446d95b33a9ea4a9
Louis King <jinglemansweep@gmail.com>
2023-01-22 21:47:49 +0000

    Working scrolly message with queue

--- src/app/experiments/scroller/__init__.py
+++ src/app/experiments/scroller/__init__.py
@@ -10,56 +10,77 @@ DESCRIPTION = "Scroller Experiment"
 TARGET_FPS = 60
 WIDTH = 640
 HEIGHT = 64
+MESSAGE_MARGIN = 10
 
 logger = logging.getLogger(NAME)
 
 logger.info("SCROLLER EXPERIMENT")
 
 pygame.init()
+
+FONT_SYSTEM = pygame.font.SysFont(None, 32)
+
 clock = pygame.time.Clock()
-
-font = pygame.font.SysFont(None, 16)
 pygame.display.set_caption(DESCRIPTION)
-
 screen_flags = RESIZABLE | SCALED
 screen = pygame.display.set_mode((WIDTH, HEIGHT), screen_flags, 16)
 
 frame = 0
+messages = []
 
 
-class Square(pygame.sprite.Sprite):
-    def __init__(self, x, y, index, width=64, height=64, color=None):
+class Message(pygame.sprite.Sprite):
+    def __init__(
+        self, position, text, font=FONT_SYSTEM, antialias=True, color=(255, 255, 255)
+    ):
         super().__init__()
-        self.width = width
-        self.height = height
-        self.index = index
-        self.color = color or (255, 255, 0)
-        self.image = pygame.Surface([self.width, self.height])
+        self.text = text
+        self.image = font.render(self.text, antialias, color)
         self.rect = self.image.get_rect()
-        self.rect[0] = x
-        self.rect[1] = y
+        self.rect[0], self.rect[1] = position[0], position[1]
 
     def update(self, frame):
-        self.image = pygame.Surface([self.width, self.height])
-        pygame.draw.rect(
-            self.image,
-            self.color,
-            pygame.Rect(0, 0, self.width, self.height),
-        )
+        self.rect[0] -= 1
 
 
-squares = pygame.sprite.Group()
+def build_message(text):
+    return Message((WIDTH, 10), text)
 
-for i in range(0, 10):
-    squares.add(Square(0 + (i * 32), 0, index=i, width=16, height=16))
+
+group_messages = pygame.sprite.Group()
+
+msg_init_1 = build_message("This is the first")
+msg_init_2 = build_message("And the second message")
+msg_init_3 = build_message("Finally, the third message")
+messages.extend([msg_init_1, msg_init_2, msg_init_3])
+logger.info(f"Messages: {messages}")
+
+
+msg_current = None
+msg_current_remain = None
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
     screen.fill((0, 0, 0))
-    squares.update(frame)
-    squares.draw(screen)
+
+    if frame % 500 == 0:
+        print("Adding new message...")
+        messages.append(build_message(f"Auto message at frame #{frame}"))
+
+    if msg_current_remain is None or msg_current_remain <= 0:
+        if len(messages):
+            msg_current = messages.pop(0)
+            msg_current_remain = msg_current.rect[2] + MESSAGE_MARGIN
+            group_messages.add(msg_current)
+    if msg_current_remain > 0:
+        msg_current_remain -= 1
+
+    group_messages.update(frame)
+    group_messages.draw(screen)
+
     pygame.display.flip()
     clock.tick(TARGET_FPS)
     frame += 1
